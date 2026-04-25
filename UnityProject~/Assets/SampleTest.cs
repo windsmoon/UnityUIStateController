1cd4d53a8693119afb4940976f51d8e5dc2fa075
windsmoon <smwindsmoon@gmail.com>
2026-04-25 22:49:13 +0800

    update

---
+++ UnityProject~/Assets/SampleTest.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleTest : MonoBehaviour
+{
+    #region fields
+    [SerializeField]
+    private Button _starLevel1Button;
+    [SerializeField]
+    private Button _starLevel2Button;
+    [SerializeField]
+    private Button _starLevel3Button;
+    [SerializeField]
+    private Button _starLevel4Button;
+    
+    [SerializeField]
+    private Button _upPosButton;
+    [SerializeField]
+    private Button _middlePosButton;
+    [SerializeField]
+    private Button _downPosButton;
+    #endregion
+}
